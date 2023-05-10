f41a57ca27776cd46ea8d0c9bc6316fb0e54e151
late fall <31011392+late-fall@users.noreply.github.com>
2023-05-09 22:02:46 -0400

    Time: 55 ms (5.22%), Space: 16.3 MB (12.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+#         paren = []
+#         for i in s:
+#             if i == "(" or i == "{" or i == "[":
+#                 paren.append(i)
+#             if i == ")" or i == "}" or i == "]":
+#                 if len(paren) == 0:
+#                     return False
+#                 p = paren.pop()
+#                 if i == ")":
+#                     if p != "(":
+#                         return False
+#                 if i == "}":
+#                     if p != "{":
+#                         return False
+#                 if i == "]":
+#                     if p != "[":
+#                         return False
+#         if len(paren) > 0:
+#             return False
+#         return True
+        
+#         NOTES
+#         common interview question
+#         try to use stack DS
+#         hashmap
+#         O(N), O(N)
+        
+#         NEETCODE
+        stack= []
+        closeToOpen = {")": "(", "]":"[","}":"{"}
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False #to make sure it's empty 
