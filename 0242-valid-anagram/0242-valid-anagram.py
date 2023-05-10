07a00f9726a7325ae1c2719571bef10f0f4b5660
late fall <31011392+late-fall@users.noreply.github.com>
2023-05-10 14:16:51 -0400

    Time: 69 ms (14.28%), Space: 17.3 MB (5.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmap_s = {}
-        hashmap_t = {}
-        if len(s) != len(t):
-            return False
-        for c in s:
-            hashmap_s[c] = 1 + hashmap_s.get(c, 0)
-        for c in t:
-            hashmap_t[c] = 1 + hashmap_t.get(c, 0)
-        return hashmap_s == hashmap_t
+#         hashmap_s = {}
+#         hashmap_t = {}
+#         if len(s) != len(t):
+#             return False
+#         for c in s:
+#             hashmap_s[c] = 1 + hashmap_s.get(c, 0)
+#         for c in t:
+#             hashmap_t[c] = 1 + hashmap_t.get(c, 0)
+#         return hashmap_s == hashmap_t
     
-    # above memory and time complexity is (S +T)
+#     # above memory and time complexity is (S +T)
     
+#     memory issues can be solved by sorting it first. 
+#     Interviews often assume sorting doesn't take extray memory usually. 
     
+        return sorted(s) == sorted(t)
