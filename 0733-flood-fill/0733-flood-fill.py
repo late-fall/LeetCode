11b69fc8adc4efafe775e65c84e29b2e2880dd15
late fall <31011392+late-fall@users.noreply.github.com>
2023-05-10 21:28:52 -0400

    Time: 91 ms (11.94%), Space: 16.6 MB (7.70%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,10 +2,10 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         row, col = len(image), len(image[0])
         prev_color = image[sr][sc]
-        if prev_color == color:
-            return image
+        # if prev_color == color: #need this to preven infinite recursion.
+        #     return image
         def newcolor(r,c):
-            if image[r][c] == prev_color:
+            if image[r][c] == prev_color and image[r][c] !=color:
                 image[r][c] = color
                 if r >= 1:
                     newcolor(r-1,c)
