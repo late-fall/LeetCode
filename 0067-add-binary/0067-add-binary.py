23b575ee5c857bc68fe093e960450ca1c154bf78
late fall <31011392+late-fall@users.noreply.github.com>
2023-05-15 00:49:29 -0400

    Time: 52 ms (14.21%), Space: 16.4 MB (9.28%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,19 +4,19 @@ class Solution:
         num_str = str(num)
         num_rev = list(num_str[::-1])
         
-        add = 0
+        carry = 0
         for i in range(len(num_rev)):
-            if add == 1:
+            if carry == 1:
                 num_rev[i] = str(int(num_rev[i]) + 1)
             if num_rev[i] == '2':
                 num_rev[i] = '0'
-                add = 1
+                carry = 1
             elif num_rev[i] == '3':
                 num_rev[i] = '1'
-                add = 1
+                carry = 1
             else:
-                add = 0
-            if i == len(num_rev) -1 and add == 1:
+                carry = 0
+            if i == len(num_rev) -1 and carry == 1:
                 num_rev += '1'
         
         print(num_rev)
