9bd729c08851a1219e5d6537e7acd8101e766a87
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-09 22:16:22 -0400

    Time: 57 ms (29.26%), Space: 17.1 MB (37.49%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,27 +7,48 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        if not root:
-            return []
+#         if not root:
+#             return []
         
-        levels = []
-        level = 0
+#         levels = []
+#         level = 0
         
-        def findLvl(root, level):
-            if root:
-                levels.append([root.val, level])
-            if root.left:
-                findLvl(root.left, level + 1)
-            if root.right:
-                findLvl(root.right, level + 1)
+#         def findLvl(root, level):
+#             if root:
+#                 levels.append([root.val, level])
+#             if root.left:
+#                 findLvl(root.left, level + 1)
+#             if root.right:
+#                 findLvl(root.right, level + 1)
         
-        findLvl(root, level)
+#         findLvl(root, level)
         
-        result = []
+#         result = []
         
-        for val in levels:
-            if len(result) < val[1] + 1:
-                result.append([val[0]])
-            else:
-                result[val[1]].append(val[0])
-        return result
+#         for val in levels:
+#             if len(result) < val[1] + 1:
+#                 result.append([val[0]])
+#             else:
+#                 result[val[1]].append(val[0])
+#         return result
+    
+#         algorithm for level order traversal is BFS. 
+#         need a queue data structure
+#         O(n) for time, visiting node one time, O(n/2) thus O(n)
+        res = []
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            qLen = len(q) # to make sure it runs 1 loop at a time
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        
+        return res
