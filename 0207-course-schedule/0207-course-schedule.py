042ef03a8743e29d728ebd8a05333042a39b3142
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-12 00:53:51 -0400

    Time: 113 ms (60.56%), Space: 19.4 MB (17.48%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+# O(N+P) number of courses + prerequisites, need to visit every node and visit edges. 
+        preHash = {i:[] for i in range(numCourses)} #create a hashmap for courses : prerequite(s)
+        for course, prereqs in prerequisites:
+            preHash[course].append(prereqs)
+        
+        visited = set()
+        
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if preHash[crs] == []:
+                return True
+            
+            visited.add(crs)
+            for pre in preHash[crs]:
+                if not dfs(pre): 
+                    return False
+            
+            visited.remove(crs)
+            preHash[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        return True
+            
+#         if len(prerequisites) >= numCourses:
+#             return False
+        
+#         courses = []
+#         pre_courses = [i[0] for i in prerequisites]
+#         print(pre_courses)
+        
+#         def checkCycle(pre):
+#             if pre[0] in courses:
+#                 return False
+#             courses.append(pre[0])
+#             checkCycle([pre[1],pre[0]])        
+        
+#         for course in prerequisites:
+#             checkCycle(course)
+#             print('testing')
+        
+#         return True
