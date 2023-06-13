59826d71e7177e0777abff8586a9dbb9543cb898
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-12 22:44:43 -0400

    Time: 288 ms (7.39%), Space: 39.5 MB (5.09%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,33 +1,32 @@
 class Trie:
 
     def __init__(self):
-        self.alpha = [[0,None] for i in range(27)]
+        self.alpha = [None] * 27
 
     def insert(self, word: str) -> None:
         if len(word) == 0:
-            self.alpha[26][0] = 1
+            self.alpha[26] = Trie()
             return
         index = ord(word[0]) - 97
-        self.alpha[index][0] = 1
-        if not self.alpha[index][1]:
-            self.alpha[index][1] = Trie()
-        self.alpha[index][1].insert(word[1:])
+        if not self.alpha[index]:
+            self.alpha[index] = Trie()
+        self.alpha[index].insert(word[1:])
 
     def search(self, word: str) -> bool:
         if len(word) == 0:
-            return self.alpha[26][0] == 1
+            return self.alpha[26] != None
         index = ord(word[0]) - 97
-        if self.alpha[index][0] == 0:
+        if self.alpha[index] == None:
             return False
-        return self.alpha[index][1].search(word[1:])
+        return self.alpha[index].search(word[1:])
 
     def startsWith(self, prefix: str) -> bool:
         if len(prefix) == 0:
             return True
         index = ord(prefix[0]) - 97
-        if self.alpha[index][0] == 0:
+        if self.alpha[index] == None:
             return False
-        return self.alpha[index][1].startsWith(prefix[1:])
+        return self.alpha[index].startsWith(prefix[1:])
         
 
 # Your Trie object will be instantiated and called as such:
