01467150efd73ca01aff280db4c113db9a175b42
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-14 23:30:36 -0400

    Time: 454 ms (7.68%), Space: 24 MB (31.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,9 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 #         BFS
-        if not grid:
-            return 0
-        
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
