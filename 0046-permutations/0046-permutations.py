f2e098411e5a27ba35fedb5eed4769f3ff3e747e
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-17 00:00:13 -0400

    Time: 45 ms (87.31%), Space: 16.7 MB (8.13%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def dfs(combo, cur):
+            if len(cur) == 0:
+                res.append(combo)
+                return
+            for i in range(len(cur)):
+                a = cur[i]
+                dfs(combo + [a], cur[:i] + cur[i+1:])
+        
+        dfs([],nums)
+        
+        return res
