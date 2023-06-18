470434388286a46a634725bb0fdda66426031a5c
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-17 21:25:43 -0400

    Time: 1206 ms (5.01%), Space: 421.9 MB (6.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,6 +1,6 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda x: x[0])
+        intervals.sort(key = lambda x : x[0])
         def dfs(i, intervals):
             if i >= len(intervals) -1 or not intervals[i+1]:
                 return intervals[:i+1]
@@ -24,3 +24,21 @@ class Solution:
                 return dfs(i+1, intervals)
         
         return dfs(0, intervals)
+    
+        
+#         # neet code solution
+#         # tip: drawing picture for itnerval problem helps a lot. 
+        
+#         # O(nLogn) -> sorting time
+#         intervals.sort(key = lambda i : i[0])
+#         output = [intervals[0]]
+        
+#         for start, end in intervals[1:]:
+#             lastEnd = output[-1][1]
+            
+#             if start <= lastEnd: #means overlapping
+#                 output[-1][1] = max(lastEnd, end)
+#             else:
+#                 output.append([start,end]) # e.g.)
+        
+#         return output
