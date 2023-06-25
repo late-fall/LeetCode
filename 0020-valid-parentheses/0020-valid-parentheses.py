85559852882152a38a49f840ff0c8138d0d64fab
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-24 21:47:31 -0400

    Time: 49 ms (45.16%), Space: 16.4 MB (13.00%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -30,12 +30,13 @@ class Solution:
 #         NEETCODE
         stack= []
         closeToOpen = {")": "(", "]":"[","}":"{"}
+        open = {"(", "[", "{"}
         for c in s:
             if c in closeToOpen:
                 if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
                     return False
-            else:
+            elif c in open:
                 stack.append(c)
         return True if not stack else False #to make sure it's empty 
