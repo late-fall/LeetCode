d56651cea6f2da7daf60b2dd30d7a140c1fc6c0d
late fall <31011392+late-fall@users.noreply.github.com>
2023-06-28 21:50:35 -0400

    Time: 43 ms (82.85%), Space: 16.3 MB (46.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,32 +1,25 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-#         dp = defaultdict(int)
-#         for i in range(m+1):
-#             dp[(1,i)] = 1
-#         for j in range(n+1):
-#             dp[(j,1)] = 1
+        dp = defaultdict(int)
+        for i in range(max(m,n)+1):
+            dp[(1,i)] = 1
+            dp[(i,1)] = 1
         
-#         if m > n:
-#             for r in range(2, m + 1):
-#                 for c in range(1,n + 1):
-#                     dp[(r,c)] = dp[(r,c -1)] + dp[(r-1,c)]  
-#         else:
-#             for c in range(2, n + 1):
-#                 for r in range(1,m + 1):
-#                     dp[(r,c)] = dp[(r,c -1)] + dp[(r-1,c)]  
-        
-#         return dp[(m,n)]
+        for r in range(2, m + 1):
+            for c in range(1,n + 1):
+                dp[(r,c)] = dp[(r,c -1)] + dp[(r-1,c)]  
+        return dp[(m,n)]
         
         #NEETcode
         # DFS
         # have cache[r][c], if reach this point, no need for DFS again. 
         # result = Right + Down
         # base position = 1, number of ways to reach ending poistion is 1.
-        row = [1] * n
+#         row = [1] * n
         
-        for i in range(m - 1):
-            newRow = [1] * n
-            for j in range(n - 2, -1, -1):
-                newRow[j] = newRow[j + 1] + row[j]
-            row = newRow
-        return row[0]
+#         for i in range(m - 1):
+#             newRow = [1] * n
+#             for j in range(n - 2, -1, -1):
+#                 newRow[j] = newRow[j + 1] + row[j]
+#             row = newRow
+#         return row[0]
