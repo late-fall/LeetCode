b695518147cf4f70d2199053ed36814f44eabaa2
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-02 22:39:21 -0400

    Time: 81 ms (95.59%), Space: 16.5 MB (35.88%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,34 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:        
+        rows = len(board)
+        cols = len(board[0])
+        if rows * cols < len(word):
+            return False
+        start = []
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    start.append((i,j))
+        
+        def checkWord(r,c,word):
+            if board[r][c] == word:
+                return True
+            if len(word) == 1 and board[r][c] != word:
+                return False
+            directions = [(0,1), (1,0), (-1, 0), (0, -1)]
+            for dr, dc in directions:
+                visited.add((r,c))
+                if (dr + r) in range(rows) and (dc + c) in range(cols) and (dr+r,dc+c) not in visited and word[1] == board[dr+r][dc+c]:
+                    result = checkWord(dr+r, dc+c, word[1:])
+                    if result:
+                        return True
+                else:
+                    visited.remove((r,c))
+                    
+        for pt in start:
+            row, col = pt
+            visited = set()
+            res = checkWord(row,col,word)
+            if res:
+                return True        
+        return False
