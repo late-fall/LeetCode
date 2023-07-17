035eebe3b07ddf91e3a16a6f961f5618ad6184ff
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-16 23:53:27 -0400

    Time: 674 ms (46.48%), Space: 17 MB (12.25%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,31 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        if n == 0:
+            return len(tasks)
+    # NEETCODE
+    # MaxHeap DS used which will allow logn
+    # total is O(N) for time and complexity
+    # no NEG maxheap, but uses minheap in python as it doesn't have native maxheap
+    # uses queue to keep track of nums of each letters and use queue. 
+    
+        count = Counter(tasks)
+        print(count)
+        maxHeap = [-cnt for cnt in count.values()]
+        print(maxHeap)
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = deque() # pairs of [-cnt, idleTime]
+
+        while maxHeap or q:
+            time += 1
+
+            if maxHeap:
+                cnt = heapq.heappop(maxHeap) + 1
+                if cnt:
+                    q.append([cnt, time + n])
+
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+
+        return time
