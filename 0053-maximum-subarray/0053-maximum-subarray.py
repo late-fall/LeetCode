f1d229aa969ed9a227ba7aebd1ca225b1fd9f0a8
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-23 22:59:01 -0400

    Time: 695 ms (74.63%), Space: 30.5 MB (71.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,6 +3,7 @@ class Solution:
 #         1) can do brute force, calculate every subarray
 #         - won't be very efficient, O(N^2)
 #         "sliding window" - O(n)
+
         maxSub = nums[0]
         curSum = 0 #initial
         
@@ -12,7 +13,7 @@ class Solution:
             curSum += n
             maxSub = max(maxSub, curSum)
         return maxSub
-    
+        
 #         first = nums[0]
 #         second = nums[0]
         
