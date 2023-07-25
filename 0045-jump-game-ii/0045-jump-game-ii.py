c16ac499f1047e3b6f1ccfa25a53dba2598ba0bf
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-24 23:30:30 -0400

    Time: 137 ms (76.86%), Space: 17.5 MB (50.58%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        idx = 0
-        cnt = 0
+        idx, cnt = 0, 0 
         while idx < len(nums) - 1:
-            max_jump = 0
-            max_idx = 0
+            max_jump, max_idx = 0, 0 
             for i in range(1,nums[idx]+1):
                 if i + idx >= len(nums):
                     break
-                if idx + nums[idx] >= len(nums) - 1:
-                    max_idx = i
-                if i + nums[i+idx] >= max_jump:
+                if idx + nums[idx] >= len(nums) - 1 or i + nums[i+idx] >= max_jump:
                     max_jump = i + nums[i+idx]
                     max_idx = i
             idx += max_idx
             cnt += 1
-            print(idx, cnt)
         return cnt
