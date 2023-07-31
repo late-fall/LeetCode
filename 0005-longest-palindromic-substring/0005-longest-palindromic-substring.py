3f0cd8408294c664a28cc9af44bce3e294deeac2
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-30 23:32:55 -0400

    Time: 475 ms (90.02%), Space: 16.4 MB (75.07%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,60 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        #Neetcode solution
-        # checking if a string is a palindrome is linear time operation
-        # brute force will be n^2, thus making (N^3)
-        # another way is consider a center and expand outward (N^2)
+        # consider odd and even palindrome, which will be calculated differently. 
         
         res = ""
-        resLen = 0
+        cur_max = 0
         
-        # odd length palindrome
+        #for odd:
         for i in range(len(s)):
             l = r = i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > resLen:
+                if r - l + 1 > cur_max:
                     res = s[l:r+1]
-                    resLen = r - l + 1
+                    cur_max = len(res)
                 l -= 1
                 r += 1
         
-        # even length palindrome
+        #for even
         for i in range(len(s)):
-            l, r = i, i + 1
+            l = i
+            r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > resLen:
+                if r - l + 1 > cur_max:
                     res = s[l:r+1]
-                    resLen = r - l + 1
+                    cur_max = len(res)
                 l -= 1
                 r += 1
-                
+        
         return res
-        
-
-#         s_original = s
-#         s_rev = s[::-1]
-#         str_len = len(s)
-#         longest = ""
-#         for i in range(str_len):
-#             cur_str = ""
-#             for j in range(str_len):
-#                 if s[j] == s_rev[j]:
-#                     cur_str += s[j]
-#                     if len(cur_str) > len(longest):
-#                         longest = cur_str
-#                 else:
-#                     cur_str = ""
-#             s = "." + s
-#         s = s_original
-#         for i in range(str_len):
-#             cur_str = ""
-#             for j in range(str_len):
-#                 if s[j] == s_rev[j]:
-#                     cur_str += s[j]
-#                     if len(cur_str) > len(longest):
-#                         longest = cur_str
-#                 else:
-#                     cur_str = ""
-#             s_rev = '.' + s_rev
-        
-#         return longest
