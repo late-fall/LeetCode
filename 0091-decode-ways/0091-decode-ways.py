71a71ff2d9ff0745555dbd27a70ee2e4a73c9bdf
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-31 21:56:41 -0400

    Time: 30 ms (99.69%), Space: 16.4 MB (60.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,8 +1,7 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         dp = collections.defaultdict(int)
-        dp[0] = 1
-        dp[1] = 1
+        dp[0], dp[1] = 1, 1
                 
         for i in range(len(s)):
             if s[i] == "0":
@@ -10,7 +9,7 @@ class Solution:
                     return 0
                 else:
                     dp[i+1] = dp[i-1]
-            elif s[i-1] == "1" or (s[i-1] == "2" and int(s[i]) < 7 and int(s[i]) >0):
+            elif s[i-1] == "1" or (s[i-1] == "2" and s[i] in "123456"):
                 dp[i+1] = dp[i-1] + dp[i]
             else:
                 dp[i+1] = dp[i]
