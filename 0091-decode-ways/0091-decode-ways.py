9438ca9d3656c79465fccf333f1db0f5c5826eaa
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-31 21:52:17 -0400

    Time: 43 ms (82.51%), Space: 16.4 MB (60.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,20 +2,14 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         dp = collections.defaultdict(int)
         dp[0] = 1
-        if s[0] == "0":
-            return 0
-        else:
-            dp[1] = 1
-        
-        if len(s) == 1:
-            return dp[1]
-        
-        for i in range(1,len(s)):
+        dp[1] = 1
+                
+        for i in range(len(s)):
             if s[i] == "0":
-                if s[i-1] == "1" or s[i-1] == "2":
-                    dp[i+1] = dp[i-1]
-                else:
+                if i == 0 or s[i-1] not in "12":
                     return 0
+                else:
+                    dp[i+1] = dp[i-1]
             elif s[i-1] == "1" or (s[i-1] == "2" and int(s[i]) < 7 and int(s[i]) >0):
                 dp[i+1] = dp[i-1] + dp[i]
             else:
