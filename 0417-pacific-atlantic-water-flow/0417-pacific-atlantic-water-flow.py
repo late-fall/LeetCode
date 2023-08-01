0388b7228ae993f20c89b48556c4960390318c56
late fall <31011392+late-fall@users.noreply.github.com>
2023-07-31 23:35:44 -0400

    Time: 2119 ms (7.88%), Space: 18.2 MB (22.14%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,67 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+        flows = [[0 for y in range(cols)] for x in range(rows)]        
+        
+        def pacificBFS(row,col):
+            q = deque()
+            q.append((row,col))
+            pvisited = set()
+            pvisited.add((row,col))
+            
+            while q:
+                r, c = q.popleft()
+                if r - 1 < 0 or c - 1 < 0:
+                    flows[r][c] += 10
+                    return True
+                else:
+                    if (r-1,c) not in pvisited and heights[r][c] >= heights[r-1][c]:
+                        q.append((r-1,c))
+                        pvisited.add((r-1,c))
+                    if (r, c-1) not in pvisited and heights[r][c] >= heights[r][c-1]:
+                        q.append((r,c-1))
+                        pvisited.add((r,c-1))
+                    if r + 1 < rows and (r+1, c) not in pvisited and heights[r][c] >= heights[r+1][c]:
+                        q.append((r+1,c))
+                        pvisited.add((r+1,c))
+                    if c +1 <cols and (r, c+1) not in pvisited and heights[r][c] >= heights[r][c+1]:
+                        q.append((r,c+1))
+                        pvisited.add((r,c+1))
+        
+        def atlanticBFS(row,col):
+            q = deque()
+            q.append((row,col))
+            avisited = set()
+            avisited.add((row,col))
+            
+            while q:
+                r, c = q.popleft()
+                if r +1 >= rows or c + 1 >= cols:
+                    flows[r][c] += 1
+                    return True
+                else:
+                    if (r+1,c) not in avisited and heights[r][c] >= heights[r+1][c]:
+                        q.append((r+1,c))
+                        avisited.add((r+1,c))
+                    if (r, c+1) not in avisited and heights[r][c] >= heights[r][c+1]:
+                        q.append((r,c+1))
+                        avisited.add((r,c+1))
+                    if r-1 >=0 and (r-1, c) not in avisited and heights[r][c] >= heights[r-1][c]:
+                        q.append((r-1,c))
+                        avisited.add((r-1,c))
+                    if c-1 >= 0 and (r, c-1) not in avisited and heights[r][c] >= heights[r][c-1]:
+                        q.append((r,c-1))
+                        avisited.add((r,c-1))
+        
+        res = []
+        for i in range(rows):
+            for j in range(cols):
+                if pacificBFS(i,j) and atlanticBFS(i,j):
+                    res.append([i,j])
+                # if flows[i][j] >= 11:
+                #     res.append([i,j])
+        print(flows)
+        return res
+        
+        
