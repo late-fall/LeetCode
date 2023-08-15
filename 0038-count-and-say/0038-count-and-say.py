1302a25817ddef0658766a2b04f1723aa83ac631
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-15 01:27:30 -0400

    Time: 46 ms (89.37%), Space: 16.3 MB (92.03%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,13 +1,20 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        if n == 1: 
+        if n == 1:
             return "1"
-        res = "1"
-        for i in range(n-1):
-            say = ""
-            while res:
-                say += str(len(res) - len(res.lstrip(res[0]))) + res[0]
-                res = res.lstrip(res[0])      
-            res = say
-
+        prev = self.countAndSay(n - 1)
+        res = ""
+        count = 0
+        curr = prev[0]
+        
+        for num in prev:
+            if num == curr:
+                count += 1
+            else:
+                res += str(count)
+                res += str(curr)
+                curr = num
+                count = 1
+        res += str(count)
+        res += str(curr)
         return res
