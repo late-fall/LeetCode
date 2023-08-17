5e525c2721231d93c6b61d2280e0d7dc43db13d6
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-16 23:58:15 -0400

    Time: 199 ms (99.57%), Space: 17.8 MB (67.16%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -21,11 +21,12 @@ class Solution:
         l, r = 0, len(nums) - 1
         
         while l <= r:
-            mid = (l + r) // 2 # can be done by l + ((r-l) //2) to prevent overflow. 
+            mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
+        
         return -1
