f045438403c60c6c903d80e3c9f09fb55a2a9494
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-16 23:32:13 -0400

    Time: 86 ms (58.00%), Space: 16.3 MB (84.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,10 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        d1 = collections.defaultdict(int)
-        d2 = collections.defaultdict(int)
-        
         if len(s1) > len(s2):
             return False
         
-        for c in s1:
-            d1[c] += 1
-        for i in range(len(s1)):
-            d2[s2[i]] += 1
+        d1 = Counter(s1)
+        d2 = Counter(s2[:len(s1)])
         
         l = 0
         r = len(s1) - 1
@@ -18,8 +13,6 @@ class Solution:
             if d1 == d2:
                 return True
             d2[s2[l]] -= 1
-            if d2[s2[l]] == 0:
-                del d2[s2[l]]
             l += 1
             r += 1
             if r < len(s2):
