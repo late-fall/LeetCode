a2c07d86af8883aa32aa439be8c0f6825b093826
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-21 14:57:06 -0400

    Time: 66 ms (68.41%), Space: 20.4 MB (47.61%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,46 +1,47 @@
-# class MinStack:
-    
-#     def __init__(self):
-#         self.stack = []
-#         self.minStack = []
-    
-#     def push(self, val: int) -> None:
-#         self.stack.append(val)
-#         val = min(val, self.minStack[-1] if self.minStack else val)
-#         self.minStack.append(val)
-
-#     def pop(self) -> None:
-#         self.stack.pop()
-#         self.minStack.pop()
-
-#     def top(self) -> int:
-#         return self.stack[-1]
-
-#     def getMin(self) -> int:
-#         return self.minStack[-1]
-
-
+# NEETCODE solution, to keep separate mincall stack
 class MinStack:
-
+    
     def __init__(self):
-        self.minval = None
-        self.stk = []
-
+        self.stack = []
+        self.minStack = []
+    
     def push(self, val: int) -> None:
-        self.stk.append([val,self.minval])
-        if self.minval is None or self.minval > val:
-            self.minval = val
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
     def pop(self) -> None:
-        if self.minval == self.stk[-1][0]:
-            self.minval = self.stk[-1][1]
-        self.stk.pop()
+        self.stack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        return self.stk[-1][0]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minval
+        return self.minStack[-1]
+
+
+# class MinStack:
+
+#     def __init__(self):
+#         self.minval = None
+#         self.stk = []
+
+#     def push(self, val: int) -> None:
+#         self.stk.append([val,self.minval])
+#         if self.minval is None or self.minval > val:
+#             self.minval = val
+
+#     def pop(self) -> None:
+#         if self.minval == self.stk[-1][0]:
+#             self.minval = self.stk[-1][1]
+#         self.stk.pop()
+
+#     def top(self) -> int:
+#         return self.stk[-1][0]
+
+#     def getMin(self) -> int:
+#         return self.minval
 
 
 # Your MinStack object will be instantiated and called as such:
