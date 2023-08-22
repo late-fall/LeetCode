ed7dafd2f80068262f47574fa7c9329ab379ec21
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-21 22:15:39 -0400

    Time: 1077 ms (75.86%), Space: 31.6 MB (32.60%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,9 +1,9 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stk = [(temperatures[0],0)]
+        stk = []
         output = [0] * len(temperatures)
         
-        for i in range(1,len(temperatures)):
+        for i in range(len(temperatures)):
             while stk and temperatures[i] > stk[-1][0]:
                 val, idx = stk.pop()
                 output[idx] = i - idx
