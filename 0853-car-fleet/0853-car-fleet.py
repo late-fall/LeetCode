62a2b9f96f213f7202aeecf64863cd788b8346ba
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-21 22:46:00 -0400

    Time: 790 ms (75.62%), Space: 39.7 MB (17.93%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -2,15 +2,15 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         sec = []
         for i in range(len(position)):
-            sec.append([position[i],speed[i], (target - position[i]) / speed[i]])
+            sec.append([position[i], (target - position[i]) / speed[i]])
         
         sec.sort(key=lambda x: x[0])
         
         stk = []
         for s in sec:
-            while stk and stk[-1] <= s[2]:
+            while stk and stk[-1] <= s[1]:
                 stk.pop()
-            stk.append(s[2])
+            stk.append(s[1])
         
         return len(stk)
             
