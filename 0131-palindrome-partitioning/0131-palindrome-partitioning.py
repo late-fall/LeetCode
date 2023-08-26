cae5743f92c27653f6fa12d5092c389eccdb6add
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-25 23:30:07 -0400

    Time: 574 ms (61.92%), Space: 33.8 MB (23.96%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,16 +1,16 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        part = [] # current partition
+        part = []
         
-        def dfs(i):
-            if i >=len(s):
-                res.append(part.copy())
+        def backtrack(i):
+            if i == len(s):
+                res.append(part[::])
                 return
-            for j in range(i, len(s)):
+            for j in range(i,len(s)):
                 if ispal(s[i:j+1]):
                     part.append(s[i:j+1])
-                    dfs(j+1)
+                    backtrack(j+1)
                     part.pop()
         
         def ispal(s):
@@ -22,13 +22,5 @@ class Solution:
                 r -= 1
             return True
         
-        dfs(0)
+        backtrack(0)
         return res
-        
-#         def backtrack(i, subset, s):
-#             if i == len(s):
-#                 res.append(subset)
-#                 return
-            
-#             backtrack(i,subset + s[i],s[1:])
-#             backtrack(i,)
