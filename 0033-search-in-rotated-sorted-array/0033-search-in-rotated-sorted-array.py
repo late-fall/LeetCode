ef82ed00434bfeb9b1953b65e22b8d863db1ee80
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-27 21:00:35 -0400

    Time: 49 ms (61.23%), Space: 16.7 MB (33.60%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,46 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        # index, value
         l = 0
         r = len(nums) - 1
         
         while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-            if mid == l == r:
-                break
-            if nums[l] <= nums[mid]:
-                if target >= nums[l] and target < nums[mid]:
-                    r = mid - 1
+            m = (l + r) //2
+            if target == nums[m]:
+                return m
+            
+            # left portion?
+            if nums[m] >= nums[l]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
                 else:
-                    l = mid + 1
-            elif nums[l] > nums[mid]:
-                if target < nums[mid] or target >= nums[l]:
-                    r = mid - 1
+                    r = m - 1
+            
+            # right portion?
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
                 else:
-                    l = mid + 1
-        
+                    l = m + 1
         return -1
+                    
+        
+        # while l <= r:
+        #     m = (l + r) // 2
+        #     if target > nums[r]:
+        #         if target > nums[m]:
+        #             l = m + 1
+        #         elif target < nums[m]:
+        #             r = m - 1
+        #         else:
+        #             return m
+        #     elif target < nums[r]:
+        #         if target > nums[m]:
+        #             r = m - 1
+        #         elif target < nums[m]:
+        #             l = m + 1
+        #         else:
+        #             return m
+        #     else:
+        #         return r
+        # return -1
