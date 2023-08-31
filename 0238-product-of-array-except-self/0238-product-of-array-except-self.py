9994882e4fdb50d7b78ad1c1e5508af81ca1328e
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-31 00:04:49 -0400

    Time: 197 ms (69.85%), Space: 24.2 MB (39.00%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,36 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         front ,back = {}, {}
-#         total = len(nums)
-#         res = []
-               
-#         front[0] = nums[0]
-#         for i in range(1,total):
-#             front[i] = front[i-1] * nums[i]
-        
-#         back[total-1] = nums[total-1]
-#         for i in range(total-2, -1, -1):
-#             back[i] = back[i+1] * nums[i]
-        
-#         for i in range(len(nums)):
-#             if i < 1:
-#                 res.append(back[i+1])
-#             elif i >=len(nums) -1:
-#                 res.append(front[i-1])
-#             else:
-#                 res.append(back[i+1] * front[i-1])
-        
-#         return res
-    
-        res = [1] * (len(nums))
-        
         prefix = 1
+        res = [1] * len(nums)
         for i in range(len(nums)):
-            res[i] = prefix
+            res[i] = prefix * res[i]
             prefix *= nums[i]
         
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
-            res[i] *= postfix
+        for i in range(len(nums)-1,-1,-1):
+            res[i] = postfix * res[i]
             postfix *= nums[i]
+        
         return res
