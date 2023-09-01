c078a100b56320a7b3084ae9122e434f9097309f
late fall <31011392+late-fall@users.noreply.github.com>
2023-08-31 21:58:38 -0400

    Time: 114 ms (75.47%), Space: 18.2 MB (62.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,35 +1,55 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        waterset = set()
-        gr, water = 0, 0
+#         waterset = set()
+#         gr, water = 0, 0
         
-        # run front to back. 
-        l = 0 
-        r = l + 1
-        while r < len(height):
-            if height[r] >= height[l]:
-                if r - l >1:
-                    waterset.add((l,r))
-                    water += (r - l - 1) * min(height[r],height[l]) - gr
-                    gr = 0
-                l = r
-            else: 
-                gr += height[r]
-            r += 1
+#         # run front to back. 
+#         l = 0 
+#         r = l + 1
+#         while r < len(height):
+#             if height[r] >= height[l]:
+#                 if r - l >1:
+#                     waterset.add((l,r))
+#                     water += (r - l - 1) * min(height[r],height[l]) - gr
+#                     gr = 0
+#                 l = r
+#             else: 
+#                 gr += height[r]
+#             r += 1
         
-        # run back to front. Make sure to omit the same water that was calculated
-        b = len(height) - 1
-        f = b - 1
-        gr = 0
-        while f >= 0:
-            if height[f] >= height[b]:
-                if b - f > 1:
-                    if (f,b) not in waterset:
-                        water += (b - f - 1) * min(height[b],height[f]) - gr
-                    gr = 0
-                b = f
+#         # run back to front. Make sure to omit the same water that was calculated
+#         b = len(height) - 1
+#         f = b - 1
+#         gr = 0
+#         while f >= 0:
+#             if height[f] >= height[b]:
+#                 if b - f > 1:
+#                     if (f,b) not in waterset:
+#                         water += (b - f - 1) * min(height[b],height[f]) - gr
+#                     gr = 0
+#                 b = f
+#             else:
+#                 gr += height[f]
+#             f -= 1
+        
+#         return water
+
+        if not height: 
+            return 0
+        
+        l, r = 0, len(height) - 1
+        ltMax, rtMax = height[l], height[r]
+        res = 0
+        
+        while l < r:
+            if ltMax < rtMax:
+                l += 1
+                ltMax = max(ltMax, height[l])
+                res += ltMax - height[l]
             else:
-                gr += height[f]
-            f -= 1
-        
-        return water
+                r -= 1
+                rtMax = max(rtMax, height[r])
+                res += rtMax - height[r]
+        return res
+            
+            
