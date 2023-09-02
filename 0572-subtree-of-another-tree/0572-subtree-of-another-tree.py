081c2d391b2182643475b127e617f0910367ea05
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-02 01:43:45 -0400

    Time: 101 ms (83.39%), Space: 17.5 MB (77.07%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,24 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    def isSubtree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
         
-        def checksub(root,subRoot):
-            if not root and not subRoot:
-                return True
-            if not root or not subRoot or root.val != subRoot.val:
-                return False
-            return checksub(root.left,subRoot.left) and checksub(root.right,subRoot.right)
+#         def checksub(root,subRoot):
+#             if not root and not subRoot:
+#                 return True
+#             if not root or not subRoot or root.val != subRoot.val:
+#                 return False
+#             return checksub(root.left,subRoot.left) and checksub(root.right,subRoot.right)
         
-        q = deque()
-        q.append(root)
-        while q:
-            node = q.popleft()
-            if node:
-                if node.val == subRoot.val:
-                    if checksub(node,subRoot):
-                        return True
-                q.append(node.left)
-                q.append(node.right)
+#         q = deque()
+#         q.append(root)
+#         while q:
+#             node = q.popleft()
+#             if node:
+#                 if node.val == subRoot.val:
+#                     if checksub(node,subRoot):
+#                         return True
+#                 q.append(node.left)
+#                 q.append(node.right)
+#         return False
+        
+        if not t:
+            return True
+        if not s:
+            return False
+        if self.sameTree(s,t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+    def sameTree(self, s, t):
+        if not s and not t:
+            return True
+        if s and t and s.val == t.val:
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
         return False
-        
