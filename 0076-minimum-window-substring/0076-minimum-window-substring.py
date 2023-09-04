915194922e73e3b175666c7871768b0967a42995
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-03 22:39:32 -0400

    Time: 102 ms (85.77%), Space: 17.2 MB (59.41%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,79 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "": 
+            return ""
+        
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c,0)
+        
+        have, need = 0, len(countT)
+        res, resLen = [-1,-1], float('inf')
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            
+            if c in countT and window[c] == countT[c]:
+                have += 1
+                
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        
+        l, r = res
+        return s[l:r+1] if resLen != float('inf') else ""
+        
+        
+#         if len(t) > len(s):
+#             return ""
+#         sd, td = Counter(s), Counter(t)
+#         if len(t) == len(s):
+#             return s if sd == td else ""
+        
+#         l = 0
+#         r = len(t) - 1
+        
+#         def checksub(sd, td):
+#             for c in td:
+#                 if td[c] > sd[c]:
+#                     return False
+#             return True
+        
+#         shortest = 10**5 + 1
+#         res_l, res_r = -1, -1
+        
+#         sdic = Counter(s[l:r+1])
+        
+#         while r < len(s):
+#             while s[l] not in td:
+#                 sdic[s[l]] -= 1
+#                 l += 1
+#                 r += 1
+#                 if r < len(s):
+#                     sdic[s[r]] += 1
+#                 if l == len(s):
+#                     break
+#             if checksub(sdic,td):
+#                 prev_r = r
+#                 if len(s[l:r+1]) < shortest:
+#                     res_l = l
+#                     res_r = r
+#                     shortest = len(s[l:r+1])
+#                 sdic[s[l]] -= 1
+#                 l += 1
+#                 r = l + len(t) - 1
+#                 for i in range(prev_r, r, -1):
+#                     sdic[s[i]] -= 1
+#                 # sdic = Counter(s[l:r+1])
+#                 continue
+#             r += 1
+#             if r < len(s):
+#                 sdic[s[r]] += 1
+        
+#         return s[res_l:res_r+1]
