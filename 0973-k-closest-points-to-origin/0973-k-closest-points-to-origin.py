56a1a5a9f135f748229e67b3b061ec43bc4fe8b2
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-04 00:00:33 -0400

    Time: 741 ms (50.44%), Space: 22.9 MB (42.78%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,45 +1,14 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-#         using minHeap
-#         Heapify - linear time algorithm
-# add them to minHeap and pop k times.
-#         minHeap = []
-#         for x,y in points:
-#             dist = (x**2) + (y**2)
-#             minHeap.append([dist,x,y])
+        pts = []
         
-#         heapq.heapify(minHeap) #heapify the list
-#         res = []
-#         while k > 0:
-#             dist, x, y = heapq.heappop(minHeap)
-#             res.append([x,y])
-#             k -= 1
-#         return res
-        res = []
         for x,y in points:
-            dist = x**2 + y**2
-            res.append([dist,x,y])
+            d = x**2 + y **2
+            heapq.heappush(pts,[d,[x,y]])
         
-        res.sort(key=lambda x:x[0])
-        print(res)
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(pts)[1])
         
-        return [[i[1],i[2]] for i in res][:k]
-    
-        
-# #         works but goes overtime
-#         res = [(points[0], points[0][0]**2 + points[0][1]**2)]
-#         final = [points[0]]
-#         for pt in points[1:]:
-#             dist = pt[0]**2 + pt[1]**2
-#             if len(res) > k:
-#                 if res[k][1] < dist:
-#                     continue
-#             i = 0
-#             while i <len(res) and res[i][1] < dist:
-#                 i+=1
-#                 if i > k:
-#                     break
-#             if i <= k:
-#                 res.insert(i, (pt,dist))
-#                 final.insert(i,pt)
-#         return final[:k]
+        return res
+            
