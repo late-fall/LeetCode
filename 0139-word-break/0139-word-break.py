bb93afe724a05af01a2954a19e507305c6b82073
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-23 00:58:53 -0400

    Time: 42 ms (71.31%), Space: 16.3 MB (80.86%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,43 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-
-        tested = set()
-        que = []
-        for word in wordDict:
-            if s[:len(word)] == word:
-                que.append(s[len(word):])
-                                
-        while que:
-            item = que.pop(0)
-            if len(item) == 0:
-                return True
-            for word in wordDict:
-                i = len(word)
-                if item[:i] == word:
-                    if item[i:] not in tested:
-                        que.append(item[i:])
-                        tested.add(item[i:])
-                    
-        return False
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
         
-        
-        ##my struggle to solve using hashmap and DFS
-#         words = {}
-#         for w in wordDict:
-#             if w[0] not in words:
-#                 words[w[0]] = [w]
-#             else:
-#                 words[w[0]].append(w)
-        
-        # def dfs(s):
-        #     print(s)
-        #     if len(s) == 0:
-        #         return True
-        #     if s[0] not in words:
-        #         return
-        #     for word in wordDict:
-        #         if s[:len(word)] == word:
-        #             res = dfs(s[len(word):])
-        #             if res:
-        #                 return True
-        # return dfs(s)
+        for i in range(len(s), -1, -1):
+            for w in wordDict:
+                if len(w) + i <= len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i+len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
