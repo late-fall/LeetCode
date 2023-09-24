be62303c9e4e3ce89502925d9d8e84f82e70d84c
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-24 00:24:11 -0400

    Time: 376 ms (85.00%), Space: 17.9 MB (67.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -8,7 +8,7 @@ class Solution:
         dp = set()
         dp.add(0)
         
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)):
             nextDP = set()
             for t in dp:
                 if (t + nums[i]) == val:
