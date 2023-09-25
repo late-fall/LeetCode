b6cbdfd9b1a13b2327d5d88971934a8c81a6ee97
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-24 23:22:54 -0400

    Time: 675 ms (55.09%), Space: 73.5 MB (89.63%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,51 +1,18 @@
-# class TimeMap:
-
-#     def __init__(self):
-#         self.stamps = {}
-#         self.time_stamps = {}
-
-#     def set(self, key: str, value: str, timestamp: int) -> None:
-#         self.stamps[key] = (value, timestamp)
-#         self.time_stamps[(key, timestamp)] = value
-
-#     def get(self, key: str, timestamp: int) -> str:
-#         if (key, timestamp) in self.time_stamps:
-#             return self.time_stamps[(key, timestamp)]
-#         else:
-#             if key in self.stamps:
-#                 time = self.stamps[key][1]
-#             else:
-#                 return ""
-#             while timestamp < time or (key, time) not in self.time_stamps:
-#                 time -= 1
-#                 if time <0:
-#                     return ""
-                
-#         return self.time_stamps[(key, time)]
-
-
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
-
-# neetcode solution:
-
 class TimeMap:
 
     def __init__(self):
-        self.store = {} #key = string, values = [list of (value, timestamp)]
+        self.keyval = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value,timestamp])
-        
+        if key in self.keyval:
+            self.keyval[key].append((value,timestamp))
+        else:
+            self.keyval[key] = [(value, timestamp)]
+
     def get(self, key: str, timestamp: int) -> str:
         res = ""
-        values = self.store.get(key, [])
+        values = self.keyval.get(key, [])
         
-        # binary search
         l, r = 0, len(values) - 1
         while l <= r:
             m = (l + r) // 2
@@ -55,3 +22,9 @@ class TimeMap:
             else:
                 r = m - 1
         return res
+        
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
