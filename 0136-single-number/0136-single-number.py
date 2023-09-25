4cc4eee078ad25546a01ac57d00937068bd81882
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-24 22:42:34 -0400

    Time: 131 ms (34.60%), Space: 19.2 MB (19.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i, n in enumerate(nums):
-            if n not in nums[i+1:] + nums[:i]:
-                return n
+        # for i, n in enumerate(nums):
+        #     if n not in nums[i+1:] + nums[:i]:
+        #         return n
+        
+        visit = set()
+        for n in nums:
+            if n in visit:
+                visit.remove(n)
+                continue
+            visit.add(n)
+        return list(visit)[0]
             
