dad2a4be0e04a4db86e0eab9e56cfc6ecd8d3d2e
late fall <31011392+late-fall@users.noreply.github.com>
2023-09-26 23:49:22 -0400

    Time: 125 ms (96.08%), Space: 21.2 MB (32.60%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,8 +5,6 @@ class Solution:
         intervals.sort(key=lambda x:x[0])
         start = intervals[0][0]
         end = intervals[0][1]
-        if len(intervals) < 2:
-            return intervals
         
         for i in range(1, len(intervals)):
             if intervals[i][0] <= end: # overlapping
