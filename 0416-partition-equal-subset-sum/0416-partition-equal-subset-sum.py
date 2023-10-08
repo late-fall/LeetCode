51a014a1a3996225b1d7095d8301432dcfc5be10
late fall <31011392+late-fall@users.noreply.github.com>
2023-10-07 23:51:08 -0400

    Time: 1917 ms (27.19%), Space: 32.5 MB (36.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,23 +1,48 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         #NEETCODE solution
-        total = sum(nums)
-        if total % 2 == 1:
-            return False
-        val = total // 2
-        dp = set()
-        dp.add(0)
+#         total = sum(nums)
+#         if total % 2 == 1:
+#             return False
+#         val = total // 2
+#         dp = set()
+#         dp.add(0)
         
-        for n in nums:
-            nextDP = set()
-            for t in dp:
-                if (t + n) == val:
-                    return True
-                nextDP.add(t+n)
-                nextDP.add(t)
-            dp = nextDP
-        return False
+#         for n in nums:
+#             nextDP = set()
+#             for t in dp:
+#                 if (t + n) == val:
+#                     return True
+#                 nextDP.add(t+n)
+#                 nextDP.add(t)
+#             dp = nextDP
+#         return False
 
+        t = sum(nums)
+        if t % 2 == 1:
+            return False
+        t = t // 2
+        n = len(nums)
+        
+        dp = [[False for x in range(t+1)] for y in range(n)]
+        
+        for i in range(n):
+            dp[i][0] = True
+        
+        for i in range(1,t+1):
+            dp[0][i] = (nums[0] == i)
+            # for 1 item only, see if it's same as the target.
+        
+        for i in range(1, n):
+            for s in range(1, t+1):
+                if dp[i-1][s]:
+                    dp[i][s] = True
+                elif s >= nums[i]:
+                    dp[i][s] = dp[i-1][s - nums[i]]
+        
+        return dp[n-1][t]
+        
+            
 #         def dfs(i, target):
 #             if target < 0 or i < 0:
 #                 cache[target] = False
