95ad10bf93a56c526e5839093114c21f151124bf
late fall <31011392+late-fall@users.noreply.github.com>
2023-10-08 21:03:12 -0400

    Time: 43 ms (26.23%), Space: 16.4 MB (39.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,25 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = defaultdict(int)
-        for i in range(max(m,n)+1):
-            dp[(1,i)] = 1
-            dp[(i,1)] = 1
+        dp = [[0 for x in range(n)] for y in range(m)]
         
-        for r in range(2, m + 1):
-            for c in range(1,n + 1):
-                dp[(r,c)] = dp[(r,c -1)] + dp[(r-1,c)]  
-        return dp[(m,n)]
+        dp[0][0] = 1
+        for i in range(1,m):
+            dp[i][0] = 1
+        for i in range(1,n):
+            dp[0][i] = 1
         
-        #NEETcode
-        # DFS
-        # have cache[r][c], if reach this point, no need for DFS again. 
-        # result = Right + Down
-        # base position = 1, number of ways to reach ending poistion is 1.
-#         row = [1] * n
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
         
-#         for i in range(m - 1):
-#             newRow = [1] * n
-#             for j in range(n - 2, -1, -1):
-#                 newRow[j] = newRow[j + 1] + row[j]
-#             row = newRow
-#         return row[0]
+        
+        return dp[m-1][n-1]
