e2d2c959a1044196f74bd4c06ece72a8446ad031
late fall <31011392+late-fall@users.noreply.github.com>
2023-11-18 22:27:39 -0500

    Time: 43 ms (43.07%), Space: 16.5 MB (47.43%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,41 +1,9 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # for each item, we have a choice of including it or not
-        # brute force is O(2^n) -> number of subsets
+        res = [[]]
         
-        # backtracking can be used. 
-        res = []
-        subset = [] # to add each to result
-        def backtrack(n):
-            if n == len(nums):
-                res.append(subset.copy()) #subset will be modified otherwise.
-                return
-            
-            # add the num
-            subset.append(nums[n])
-            backtrack(n+1)
-            
-            subset.pop()
-            backtrack(n+1)
-                    
-        backtrack(0)
+        for i in range(len(nums)):
+            for j in range(len(res)):
+                res.append(res[j] + [nums[i]])
         
         return res
-        
-#         nums_idx = defaultdict(list)
-#         subsets = []
-        
-#         # create a dictionary for location of each element in nums
-#         for i,n in enumerate(nums):
-#             subsets.append([n])
-#             nums_idx[n] = i
-        
-#         # attach one element at a time starting from the last location.
-#         while len(subsets[-1]) < len(nums):
-#             for s in subsets:
-#                 for n in nums[nums_idx[s[-1]]+1:]:
-#                     subsets.append(s + [n])
-        
-#         subsets.append([])
-        
-#         return subsets
