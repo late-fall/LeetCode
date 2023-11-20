6e3d4a242eb7f8eddd3d83e54ad2fd5a3fa34e58
late fall <31011392+late-fall@users.noreply.github.com>
2023-11-19 23:03:39 -0500

    Time: 331 ms (49.64%), Space: 32.1 MB (40.26%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,23 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = [[0 for x in range(amount+1)] for y in range(n)]
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n)]
         
         for i in range(n):
             dp[i][0] = 1
         
-        for i in range(amount+1):
-            if i % coins[0] == 0:
-                dp[0][i] = 1
-            else:
-                dp[0][i] = 0
+        for i in range(amount + 1):
+            dp[0][i] = 1 if (coins[0] <= i and i % coins[0] == 0 or i==0) else 0
         
-        for c in range(1,n):
-            for amt in range(1,amount+1):
-                change1 = 0
-                if coins[c] <= amt:
-                    change1 = dp[c][amt-coins[c]]
-                change2 = dp[c-1][amt]
-                dp[c][amt] = change1 + change2               
-        
+        for i in range(1,n):
+            for j in range(1,amount + 1):
+                if j >= coins[i]:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+            
         return dp[n-1][amount]
