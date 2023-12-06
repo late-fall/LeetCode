29b3b14707b2123b12bbafe6d72cfa2a020a7ad8
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-05 22:25:58 -0500

    Time: 110 ms (73.31%), Space: 18.3 MB (46.90%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,6 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
+        # height of water level at one point is min(max height of left, max height of right) - current height.
+        # one way to solve is by creating an array of maxLeft, maxRight, and min(maxL, maxR)
+        
+        # trick is to use two pointers, left and right. Starting from each end.
+        # set current leftMax and rightMax which is basically their respective values for now.
+        # shift pointer which the max is smaller. 
+        # for e.g., if ltMax <rtMax, we don't need to know what rtMax is since it doesn't affect equation.
+        # water height will still be min(leftMax, rightMax) - height[i] where leftMax - height[i]
+        
         l, r = 0, len(height) - 1
         ltMax, rtMax = height[l], height[r]
         res = 0
