6b27258e9f8162b202b27b416fcf21ff63f687c7
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-06 20:50:01 -0500

    Time: 5537 ms (12.68%), Space: 17.4 MB (97.19%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,9 +2,7 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        
-        # words = [(x, Counter(y)) for x, y in zip(wordList, wordList)]
-    
+            
         def dif_single(w1, w2):
             cnt = 0
             for i in range(len(w1)):
@@ -19,13 +17,11 @@ class Solution:
         q = deque()
         q.append(beginWord)
         res = 1
-        found = False
         
         while q:
             for _ in range(len(q)):
                 wordCount = q.popleft()
                 if endWord == wordCount:
-                    found = True
                     return res
                 for w in wordList:
                     if dif_single(wordCount, w) and w not in q:
