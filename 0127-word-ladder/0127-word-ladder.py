da7655f78d899a26eae4a41059cf4f380fadaf7a
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-06 21:12:01 -0500

    Time: 403 ms (37.90%), Space: 18.1 MB (64.18%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,33 +1,50 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
-            
-        def dif_single(w1, w2):
-            cnt = 0
-            for i in range(len(w1)):
-                if w1[i] != w2[i]:
-                    cnt += 1
-                    if cnt > 1:
-                        return False
-            return cnt == 1
-        
         wordList = set(wordList)
-        
-        q = deque()
-        q.append(beginWord)
+        queue = collections.deque([beginWord])
         res = 1
-        
-        while q:
-            for _ in range(len(q)):
-                word = q.popleft()
-                if endWord == word:
+        while queue:
+            print(queue)
+            for _ in range(len(queue)):
+                word = queue.popleft()
+                if word == endWord:
                     return res
-                for w in wordList:
-                    if dif_single(word, w) and w not in q:
-                        q.append(w)
-            for w in q:
-                wordList.remove(w)
+                for i in range(len(word)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        next_word = word[:i] + c + word[i+1:]
+                        if next_word in wordList:
+                            wordList.remove(next_word)
+                            queue.append(next_word)
             res += 1
-        
         return 0
+#         if endWord not in wordList:
+#             return 0
+            
+#         def dif_single(w1, w2):
+#             cnt = 0
+#             for i in range(len(w1)):
+#                 if w1[i] != w2[i]:
+#                     cnt += 1
+#                     if cnt > 1:
+#                         return False
+#             return cnt == 1
+        
+#         wordList = set(wordList)
+        
+#         q = deque()
+#         q.append(beginWord)
+#         res = 1
+        
+#         while q:
+#             for _ in range(len(q)):
+#                 word = q.popleft()
+#                 if endWord == word:
+#                     return res
+#                 for w in wordList:
+#                     if dif_single(word, w) and w not in q:
+#                         q.append(w)
+#             for w in q:
+#                 wordList.remove(w)
+#             res += 1
+        
+#         return 0
