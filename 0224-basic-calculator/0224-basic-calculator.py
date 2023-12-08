311e0bc696e699339213928a596002d8433dfe9d
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-07 20:20:46 -0500

    Time: 88 ms (47.18%), Space: 74.6 MB (5.32%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,29 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def addstk(op, num):
-            if op == '+':
-                stk.append(num)
-            else:
-                stk.append(-num)
+        # goal is to add as an element to a stack and finally calculate its sum
+        # if it has + or -, create a new element, * or / will be calculated right away to the last stack element.
+        # use recursion when new () happens so treat it as a new function that spits out value at the end. 
         
-        i = 0
-        num = 0
+        def stk_update(num, op):
+            stk.append(num) if op == '+' else stk.append(-num)
+        
+        n = 0
         stk = []
-        sign = '+'
+        op = '+'
+        i = 0
         
         while i < len(s):
             if s[i].isdigit():
-                num = num * 10 + int(s[i])
+                n = n * 10 + int(s[i])
             elif s[i] in '+-':
-                addstk(sign, num)
-                num, sign = 0, s[i]
+                stk_update(n, op)
+                n = 0
+                op = s[i]
             elif s[i] == '(':
-                num, j = self.calculate(s[i +1:])
-                i = i + j
+                n, j = self.calculate(s[i+1:])
+                i += j
             elif s[i] == ')':
-                addstk(sign, num)
+                stk_update(n, op)
                 return sum(stk), i + 1
             i += 1
-        addstk(sign, num)
-        
+        stk_update(n, op)
         return sum(stk)
+        
