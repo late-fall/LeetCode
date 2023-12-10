209df4756f7724f8ce20dc753c206439d331f2fb
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-09 21:30:54 -0500

    Time: 41 ms (42.46%), Space: 16.2 MB (91.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,43 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-#         paren = []
-#         for i in s:
-#             if i == "(" or i == "{" or i == "[":
-#                 paren.append(i)
-#             if i == ")" or i == "}" or i == "]":
-#                 if len(paren) == 0:
-#                     return False
-#                 p = paren.pop()
-#                 if i == ")":
-#                     if p != "(":
-#                         return False
-#                 if i == "}":
-#                     if p != "{":
-#                         return False
-#                 if i == "]":
-#                     if p != "[":
-#                         return False
-#         if len(paren) > 0:
-#             return False
-#         return True
-        
-#         NOTES
-#         common interview question
-#         try to use stack DS
-#         hashmap
-#         O(N), O(N)
-        
-#         NEETCODE
-        parens = {")":"(","]":"[","}":"{"}
-    
         stk = []
         
-        for c in s:
-            if c in parens:
-                if not stk or parens[c] != stk.pop():
-                    return False
+        for p in s:
+            if p in '([{':
+                stk.append(p)
             else:
-                stk.append(c)
+                if not stk:
+                    return False
+                last = stk.pop()
+                if (p == ')' and last != '(') or (p==']' and last != '[') or (p == '}' and last != '{'):
+                    return False
         
-        return len(stk) == 0
-                
+        return False if stk else True
+                    
+                    
