af27a4656fee736e4424371d8ea24f15ea4de191
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-09 20:40:47 -0500

    Time: 790 ms (38.18%), Space: 30.7 MB (70.65%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,37 +1,14 @@
-import heapq
-
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        maxArea = 0
-        stk = [] # holds pair of element (idx, ht)
+    def largestRectangleArea(self, height):
+        height.append(0)
+        stack = [-1]
+        ans = 0
         
-        for i, h in enumerate(heights):
-            start = i
-            while stk and stk[-1][1] > h:
-                idx, ht = stk.pop()
-                maxArea = max(maxArea, ht * (i - idx))
-                start = idx
-            stk.append((start,h))
+        for i, h in enumerate(height):
+            while stack and h < height[stack[-1]]:
+                ht = height[stack.pop()]
+                wth = i - stack[-1] - 1
+                ans = max(ans, ht * wth)
+            stack.append(i)
         
-        for i, h in stk:
-            maxArea = max(maxArea, h * (len(heights)-i))
-        
-        return maxArea
-        
-        
-        
-#         min_ht = math(inf)
-#         max_area = 0
-#         stk = []
-#         last = -1
-        
-#         for i in range(len(heights)):
-#             min_ht = min(min_ht, heights[i])
-#             area1 = min_ht * (i - last)
-#             area2 = 
-#             if stk and stk[-1][0] <= area:
-#                 stk.pop()
-#             stk.append((area,heights[i]))
-            
-#         return area
-            
+        return ans
