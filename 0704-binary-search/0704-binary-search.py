6f47e96e2ec4941048e5f22bbdbafdbe8cb7a6e1
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-09 22:44:23 -0500

    Time: 212 ms (67.19%), Space: 17.9 MB (47.64%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,32 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-#         mid_index = 0
-#         while len(nums) > 0 :
-#             mid = len(nums) // 2
-#             if target == nums[mid]:
-#                 return mid + mid_index
-#             elif target > nums[mid]:
-#                 mid_index += len(nums[:mid+1])
-#                 nums = nums[mid+1:]
-#             else:
-#                 target < nums[mid]
-#                 nums = nums[:mid]
-#         return -1
-    
-#     # very common question.
-    
-#     have two pointers
-#     Left, Right. get mid point using (l+r)/2
-    
         l, r = 0, len(nums) - 1
         
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
+            if nums[mid] > target:
                 r = mid - 1
+            else:
+                l = mid + 1
         
         return -1
