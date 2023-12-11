9fa301cc59a28d318c92740aa7610cec96ce03e5
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-10 23:41:23 -0500

    Time: 53 ms (78.79%), Space: 16.6 MB (15.91%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hash_r, hash_m = {},{}
-        
-        for c in ransomNote:
-            if c in hash_r:
-                hash_r[c] += 1
-            else:
-                hash_r[c] = 1
-        
-        for c in magazine:
-            if c in hash_m:
-                hash_m[c] += 1
-            else:
-                hash_m[c] = 1
-        
-        for c in hash_r:
-            if hash_r[c] > hash_m.get(c,0):
-                return False
-        return True
+        return Counter(ransomNote) <= Counter(magazine)
