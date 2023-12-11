f9ca6a21ae885c0f47ad5575c70e17c38265afac
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-10 20:11:23 -0500

    Time: 71 ms (68.23%), Space: 20.7 MB (74.35%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,30 +7,10 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         while root:
-#             que = deque()
-#             que.append(root)
-            
-#             while que:
-#                 node = que.popleft()
-#                 minval = min(p.val,q.val)
-#                 maxval = max(p.val,q.val)
-#                 if node: 
-#                     if p.val == node.val or q.val == node.val:
-#                         return node
-#                     if minval < node.val and maxval > node.val:
-#                         return node
-#                     que.append(node.left)
-#                     que.append(node.right)
-        
-        def dfs(root):
-            if not root:
-                return
-            if root.val == p.val or root.val == q.val:
-                return root
-            if min(p.val,q.val) < root.val and max(p.val,q.val) > root.val:
-                return root
-            return dfs(root.left) or dfs(root.right)
-        
-        return dfs(root)
-                
+        if not root:
+            return None
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
