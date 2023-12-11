45cc9c961c0d94c3c0174661bd1e512dc529f15f
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-10 19:58:09 -0500

    Time: 77 ms (46.65%), Space: 16.8 MB (14.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,21 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        visited = set()
         rows = len(image)
         cols = len(image[0])
-        original = image[sr][sc]
-
-        def fill(r, c, color):
-            if r >= rows or r < 0 or c >= cols or c < 0 or (r,c) in visited or image[r][c] != original:
+        visited = set()
+        initial = image[sr][sc]
+        
+        def dfs(r, c):
+            if r >= rows or r < 0 or c >= cols or c < 0 or (r,c) in visited or image[r][c] == color or image[r][c] != initial:
                 return
             image[r][c] = color
             visited.add((r,c))
-            fill(r+1, c, color)
-            fill(r-1, c, color)
-            fill(r, c+1, color)
-            fill(r, c-1, color)
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
         
-        fill(sr,sc, color)
-
+        dfs(sr,sc)
         return image
-            
