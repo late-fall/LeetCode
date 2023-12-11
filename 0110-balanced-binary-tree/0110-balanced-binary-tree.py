9186cba0b6bfbcc1b24ffdf0db28ea5727c2567b
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-10 20:27:05 -0500

    Time: 60 ms (24.23%), Space: 21.2 MB (41.61%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,13 +6,18 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        #try to return 2 values on separate function
-        # [boolean, ht]
-        # recursive calls which returns boolean and ht value
-        # if every false anywhere, it will return false.
+        def getHt(root):
+            if not root:
+                return -1
+            ht_lt = getHt(root.left) + 1
+            ht_rt = getHt(root.right) + 1
+            return max(ht_lt, ht_rt)
+        
         def checkBal(root):
-            if not root: return [True,0]
-            left, right = checkBal(root.left), checkBal(root.right)
-            is_balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [is_balanced, 1 + max(left[1],right[1])]
-        return checkBal(root)[0]
+            if not root:
+                return True
+            if abs(getHt(root.left) - getHt(root.right)) > 1:
+                return False
+            return checkBal(root.left) and checkBal(root.right)
+        
+        return checkBal(root)
