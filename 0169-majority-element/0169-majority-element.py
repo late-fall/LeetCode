e0131e87c1a8d583ce00341d5605322bfef5f086
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-11 20:13:05 -0500

    Time: 153 ms (45.56%), Space: 17.9 MB (13.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # hashmap = {}
-        # for n in nums:
-        #     hashmap[n] = hashmap.get(n, 0) + 1
-        #     if hashmap[n] >= len(nums) / 2:
-        #         return n
-            
-        # to make it more efficient, use Boyer_Moore algorithm:
-#        more optimal as uses less memory
-        
-        res, count = 0, 0
-        for n in nums:
-            if count == 0:
-                res = n
-            count += (1 if n == res else -1)
-        return res
+        nmap = Counter(nums)
+        for n in nmap:
+            if nmap[n] > len(nums) // 2:
+                return n
