383606a014a209194fda4fc964b24bb594631175
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-11 23:33:10 -0500

    Time: 2034 ms (5.03%), Space: 30.6 MB (10.86%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,30 +1,29 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-#         1) can do brute force, calculate every subarray
-#         - won't be very efficient, O(N^2)
-#         "sliding window" - O(n), Kadane's algorithm
-
-        maxSub = nums[0]
-        curSum = 0 #initial
         
-        for n in nums:
-            if curSum < 0:
-                curSum = 0
-            curSum += n
-            maxSub = max(maxSub, curSum)
-        return maxSub
+        def findmidmax(nums):
+            mid = len(nums) // 2
+            cur_max_lt, cur_max_rt = -float('inf'), -float('inf')
+            total_lt, total_rt = 0, 0
+            for n in range(mid-1, -1, -1):
+                total_lt += nums[n]
+                cur_max_lt = max(cur_max_lt, total_lt)
+            for n in range(mid, len(nums)):
+                total_rt += nums[n]
+                cur_max_rt = max(cur_max_rt, total_rt)
+            
+            return cur_max_lt + cur_max_rt
+            
         
-#         first = nums[0]
-#         second = nums[0]
+        def findmax(nums):
+            if len(nums) == 1:
+                return nums[0]
+            mid = len(nums) // 2
+            ltmax = findmax(nums[:mid])
+            rtmax = findmax(nums[mid:])
+            midmax = findmidmax(nums)
+            
+            return max(ltmax, rtmax, midmax)
+            
         
-#         for n in nums:
-#             if n > 0:
-#                 first += n
-#             second += n
-#             if first <second:
-#                 first = second
-                
-        
-#         return max(first,second);
-
-        
+        return findmax(nums)
