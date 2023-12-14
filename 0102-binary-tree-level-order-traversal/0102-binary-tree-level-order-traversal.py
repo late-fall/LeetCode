e0eac42d3eeb45fe569a343533747243c4053aa6
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-13 23:38:14 -0500

    Time: 40 ms (85.38%), Space: 16.9 MB (79.27%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,49 +6,18 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-#         if not root:
-#             return []
-        
-#         levels = []
-#         level = 0
-        
-#         def findLvl(root, level):
-#             if root:
-#                 levels.append([root.val, level])
-#             if root.left:
-#                 findLvl(root.left, level + 1)
-#             if root.right:
-#                 findLvl(root.right, level + 1)
-        
-#         findLvl(root, level)
-        
-#         result = []
-        
-#         for val in levels:
-#             if len(result) < val[1] + 1:
-#                 result.append([val[0]])
-#             else:
-#                 result[val[1]].append(val[0])
-#         return result
-    
-#         algorithm for level order traversal is BFS. 
-#         need a queue data structure
-#         O(n) for time, visiting node one time, O(n/2) thus O(n)
-        res = []
-        q = collections.deque()
+        q = deque()
         q.append(root)
-        
+        res = []
         while q:
-            qLen = len(q) # to make sure it runs 1 loop at a time
-            level = []
-            for i in range(qLen):
+            subset = []
+            for _ in range(len(q)):
                 node = q.popleft()
                 if node:
-                    level.append(node.val)
                     q.append(node.left)
                     q.append(node.right)
-            if level:
-                res.append(level)
+                    subset.append(node.val)
+            if subset:
+                res.append(subset)
         
         return res
