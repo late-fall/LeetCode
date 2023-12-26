5f0b8cbf692f4c742bef387b8b33cfd29b077c64
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-25 22:53:07 -0500

    Time: 81 ms (99.51%), Space: 18.9 MB (63.88%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,44 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-# O(N+P) number of courses + prerequisites, need to visit every node and visit edges. 
-        preHash = {i:[] for i in range(numCourses)} #create a hashmap for courses : prerequite(s)
-        for course, prereqs in prerequisites:
-            preHash[course].append(prereqs)
+        #topological sorting
+        graph = [[] for _ in range(numCourses)]
+        indeg = [0] * numCourses
         
-        visited = set()
-        
-        def dfs(crs):
-            if crs in visited:
-                return False
+        for c, p in prerequisites:
+            graph[p].append(c)
+            indeg[c] += 1
             
-            visited.add(crs)
-            for pre in preHash[crs]:
-                if not dfs(pre): 
-                    return False
-            
-            visited.remove(crs)
-            preHash[crs] = []
-            return True
+        q = deque()
+        # if indeg is 0, we can start from here
+        for n in range(numCourses):
+            if indeg[n] == 0:
+                q.append(n)
+                
+        num = len(q)
+        while q:
+            start = q.popleft()
+            for dest in graph[start]:
+                indeg[dest] -= 1
+                if indeg[dest] == 0:
+                    num += 1
+                    q.append(dest)
         
-        for crs in range(numCourses):
-            if not dfs(crs): return False
-        return True
-            
-#         if len(prerequisites) >= numCourses:
-#             return False
+        return num == numCourses
         
-#         courses = []
-#         pre_courses = [i[0] for i in prerequisites]
-#         print(pre_courses)
-        
-#         def checkCycle(pre):
-#             if pre[0] in courses:
-#                 return False
-#             courses.append(pre[0])
-#             checkCycle([pre[1],pre[0]])        
-        
-#         for course in prerequisites:
-#             checkCycle(course)
-#             print('testing')
-        
-#         return True
