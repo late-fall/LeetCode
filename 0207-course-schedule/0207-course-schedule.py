2766be3442205f6232050c89ad14a3c1efb983bc
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-25 22:55:16 -0500

    Time: 96 ms (71.67%), Space: 19.2 MB (49.80%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -9,19 +9,17 @@ class Solution:
             indeg[c] += 1
             
         q = deque()
-        # if indeg is 0, we can start from here
+        # if indeg is 0, we can start from here, put it in the que
         for n in range(numCourses):
             if indeg[n] == 0:
                 q.append(n)
                 
-        num = len(q)
         while q:
             start = q.popleft()
             for dest in graph[start]:
                 indeg[dest] -= 1
                 if indeg[dest] == 0:
-                    num += 1
                     q.append(dest)
         
-        return num == numCourses
+        return sum(indeg) == 0
         
