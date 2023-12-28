1301acb86a3f7b8a4bc7c2dcdcd8806377d1514d
late fall <31011392+late-fall@users.noreply.github.com>
2023-12-27 23:09:25 -0500

    Time: 64 ms (6.94%), Space: 17.8 MB (7.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,46 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        l = 0
-        r = len(nums) - 1
+        l, r = 0, len(nums) - 1
         
         while l <= r:
-            m = (l + r) //2
-            if target == nums[m]:
-                return m
-            
-            # left portion?
-            if nums[m] >= nums[l]:
-                if target > nums[m] or target < nums[l]:
-                    l = m + 1
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > nums[r]: # right side rotated
+                if target > nums[mid]:
+                    l = mid + 1
                 else:
-                    r = m - 1
-            
-            # right portion?
+                    if target < nums[l]:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
             else:
-                if target < nums[m] or target > nums[r]:
-                    r = m - 1
+                if target < nums[mid]:
+                    r = mid - 1
                 else:
-                    l = m + 1
+                    if target > nums[r]:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
         return -1
-                    
-        
-        # while l <= r:
-        #     m = (l + r) // 2
-        #     if target > nums[r]:
-        #         if target > nums[m]:
-        #             l = m + 1
-        #         elif target < nums[m]:
-        #             r = m - 1
-        #         else:
-        #             return m
-        #     elif target < nums[r]:
-        #         if target > nums[m]:
-        #             r = m - 1
-        #         elif target < nums[m]:
-        #             l = m + 1
-        #         else:
-        #             return m
-        #     else:
-        #         return r
-        # return -1
