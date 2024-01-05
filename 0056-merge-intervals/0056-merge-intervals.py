6bb5abcb3fd3167efcf775f9c9dd1c9375f73e5c
late fall <31011392+late-fall@users.noreply.github.com>
2024-01-04 21:48:46 -0500

    Time: 116 ms (98.38%), Space: 22 MB (8.60%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,7 +4,7 @@ class Solution:
         res = [[intervals[0][0], intervals[0][1]]]
         
         for i in range(1, len(intervals)):
-            if res and intervals[i][0] <= res[-1][1]:
+            if intervals[i][0] <= res[-1][1]:
                 res[-1][1] = max(res[-1][1], intervals[i][1])
             else:
                 res.append([intervals[i][0], intervals[i][1]])
