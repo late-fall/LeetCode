47e045bf74188eca875d8f9ee766889c2a53cf91
late fall <31011392+late-fall@users.noreply.github.com>
2024-01-05 22:58:56 -0500

    Time: 208 ms (44.74%), Space: 25.6 MB (15.56%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,72 +1,33 @@
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-    # from leetcode solution
-    
-        visited = set()
-        emails_accounts_map = {}
-        res = []
         
-        # Build up the graph.
-        for i, account in enumerate(accounts):
-            for j in range(1, len(account)):
-                if account[j] not in emails_accounts_map:
-                    emails_accounts_map[account[j]] = [i]
-                else:
-                    emails_accounts_map[account[j]].append(i)
-                    
-        # DFS code for traversing accounts.
-        def dfs(i, emails):
-            if i in visited:
-                return
-            visited.add(i)
-            for j in range(1, len(accounts[i])):
-                email = accounts[i][j]
-                emails.add(email)
-                for neighbor in emails_accounts_map[email]:
-                    dfs(neighbor, emails)
-                    
-        # Perform DFS for accounts and add to results.
-        for i, account in enumerate(accounts):
-            if i in visited:
-                continue
-            name, emails = account[0], set()
-            dfs(i, emails)
-            res.append([name] + sorted(emails))
-        return res
+        names = {}
+        graph = collections.defaultdict(set)
         
+        for act in accounts:
+            name = act[0]
+            for email in act[1:]:
+                graph[act[1]].add(email)
+                graph[email].add(act[1])
+                names[email] = name
         
-#         <doesn't work>
-
-#         names = set()
-#         res = []
+        print('names: ', names)
+        print('grpah: ', graph)
         
-#         for account in accounts:
-#             name = account[0]
-#             emails = account[1:]
-            
-#             if name not in names:
-#                 res.append(account)
-#                 names.add(name)
-#             else:
-#                 temp2 =[]
-#                 for acc in res:
-#                     same = False
-#                     temp = []
-#                     for email in emails:
-#                         if email in acc[1:]:
-#                             same = True
-#                         else:
-#                             temp.append(email)
-#                     if same:
-#                         acc.extend(temp)
-#                     else:
-#                         temp2 = [name] + temp
-#                 if len(temp2) > 0 and temp2[0] not in names:
-#                     res.append(temp2)
+        connected, visited, i = collections.defaultdict(list), set(), 0
         
-#         result = []
+        def dfs(node, i):
+            connected[i].append(node)
+            visited.add(node)
+            for nb in graph[node]:
+                if nb not in visited:
+                    dfs(nb, i)
         
-#         for item in res:
-#             result.append(sorted(list(set(item))))
+        for email in graph:
+            if email not in visited:
+                dfs(email, i)
+                i += 1
         
-#         return sorted(result)
+        print('connected components: ', connected)
+                
+        return [[names[val[0]]] + sorted(val) for _, val in connected.items()]
