3a92547db3e2200a82d13ae1e8813d1b14f0a720
late fall <31011392+late-fall@users.noreply.github.com>
2024-01-08 21:03:33 -0500

    Time: 126 ms (85.56%), Space: 21.9 MB (12.40%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -7,7 +7,6 @@ class Solution:
             if not res or start > res[-1][1]:
                 res.append([start,end])
             else:
-                res[-1][0] = min(start, res[-1][0])
                 res[-1][1] = max(end, res[-1][1])
         
         return res
