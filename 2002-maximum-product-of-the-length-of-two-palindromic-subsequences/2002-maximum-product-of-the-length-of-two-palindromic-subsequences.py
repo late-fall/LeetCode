548f5336217f4b1a5b35594df76a79fa0b5189ca
late fall <31011392+late-fall@users.noreply.github.com>
2024-01-09 22:31:47 -0500

    Time: 1299 ms (79.15%), Space: 17.4 MB (45.29%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -1,32 +1,23 @@
 class Solution:
     def maxProduct(self, s: str) -> int:
-        # Brute force is OK, only small optimization possible
-        # Need to use Bit Mask
-        # for leetcodecom, ete would be 01010001000, and cdc is 00001010100
-        # to check if they are disjoint. logic & will return 0 if disjoint.
-        
-        # goes from i=1 to 2**11 -1 (which is 1111111111)
-        # hashmap[key = binary representation of the string], value is length of panlindrome.
-        # use hashmap to calculate the max value. 
-        
-        # time complexity is 4**n
+        # you can just brute force this problem as s.length max is 12
+        # use bitmask
         
         n = len(s)
-        pali = {} #binary representation of string : length
+        pal = {}
         
-        for mask in range(1, 2 ** n): # 1 << n == 2 ** n, slightly more efficient. 
+        for mask in range(1, 2**n):
             subseq = ""
             for i in range(n):
-                if mask & (2 ** i):
-                    subseq += s[i]
+                if mask & (2**i):
+                    subseq += s[n - i - 1]
             if subseq == subseq[::-1]:
-                pali[mask] = len(subseq)
+                pal[mask] = len(subseq)
         
-        #find longest
         longest = 0
-        for m1 in pali:
-            for m2 in pali:
-                if m1 & m2 == 0: # meaning they are disjoint.
-                    longest = max(longest, pali[m1] * pali[m2])
+        for mask1 in pal:
+            for mask2 in pal:
+                if mask1 & mask2 == 0:
+                    longest = max(longest, pal[mask1] * pal[mask2])
         
         return longest
